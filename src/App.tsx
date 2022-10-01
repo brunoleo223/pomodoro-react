c288b3bb24c522937ec0b21ce25cfe0f2abe0dcf
brunoleo223 <brunoleo223@gmail.com>
2022-10-01 12:12:54 -0300

    starting with styled-components

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ButtonContainer } from "./Button.styles"
+
+function App() {
+
+  return (
+    <>
+      <ButtonContainer variant="primary">Oi</ButtonContainer>
+      <ButtonContainer variant="secondary">Oi</ButtonContainer>
+      <ButtonContainer variant="danger">Oi</ButtonContainer>
+      <ButtonContainer variant="success">Oi</ButtonContainer>
+      <ButtonContainer>Oi</ButtonContainer>
+    </>
+  )
+}
+
+export default App
